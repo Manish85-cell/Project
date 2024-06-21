43b4357532bc17802e88599965abc3e7fca208c8
Manish <mupreti2002@gmail.com>
2024-06-21 21:35:33 +0530

    FRONTEND BUILD AND API FOR BACKEND HANDLING BUILD AND LOGIN/SIGNUP BUILD

---
+++ heat_wave/oj/src/components/Contest.js
@@ -0,0 +1,12 @@
+import React , {Component} from "react"
+import '../../static/css/index';
+
+export default class Contest extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (<h1>This is Contest page</h1>)
+    }
+}
---
+++ heat_wave/oj/src/components/Problem.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react"
+
+export default class Problem extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <>
+                <div>
+                <table class="table table-hover" id="tab">
+                            <thead>
+                                <tr>
+                                    <th>Status</th>
+                                    <th>Problem</th>
+                                    <th>Acceptance</th>
+                                    <th>Difficulty Level</th>
+                                  
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Solved</td>
+                                    <td>Two Sum  with target value</td>
+                                    <td>70%</td>
+                                    <td>Medium</td>                                 
+                                </tr>
+                                <tr>
+                                    <td>Solved</td>
+                                    <td>Two Sum  with target value</td>
+                                    <td>70%</td>
+                                    <td>Medium</td>                                 
+                                </tr>
+                                <tr>
+                                    <td>Solved</td>
+                                    <td>Two Sum  with target value</td>
+                                    <td>70%</td>
+                                    <td>Medium</td>                                 
+                                </tr>
+                                <tr>
+                                    <td>Solved</td>
+                                    <td>Two Sum  with target value</td>
+                                    <td>70%</td>
+                                    <td>Medium</td>                                 
+                                </tr>
+                                <tr>
+                                    <td>Solved</td>
+                                    <td>Two Sum  with target value</td>
+                                    <td>70%</td>
+                                    <td>Medium</td>                                 
+                                </tr>
+                            </tbody>
+                  </table>
+                </div>
+        </>
+    }
+}
+
